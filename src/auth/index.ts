4ff0f55ffe93ab1a91a8047098fbdad737808f06
Hoai Thai <khanhhoai95@gmail.com>
2022-01-30 11:19:46 +0700

    refactor: remove unused code

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,10 +1,9 @@
 import { FastifyInstance } from "fastify"
 import { buildGetCurrentUserHandler, GetCurrentUserResponse, getGetCurrentUserOptions } from "./getCurrentUser"
 import { buildSignInHandler, signInOptions } from "./signIn"
-import { buildSignUpHandler, buildSignUpRoute, signUpOptions } from "./signUp"
+import { buildSignUpHandler, signUpOptions } from "./signUp"
 
 export const authRoutes = async (fastify: FastifyInstance) => {
-  // fastify.route(buildSignUpRoute(fastify))
   fastify.post("/auth/sign-up", signUpOptions, buildSignUpHandler(fastify))
   fastify.post("/auth/sign-in", signInOptions, buildSignInHandler(fastify))
   fastify.get<{ Reply: GetCurrentUserResponse }>(
--- src/auth/signUp.ts
+++ src/auth/signUp.ts
@@ -1,5 +1,5 @@
 import { User } from "@prisma/client"
-import { FastifyInstance, FastifyReply, FastifyRequest, HTTPMethods } from "fastify"
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
 
 export type SignUpRequest = {
   username: string
@@ -10,43 +10,6 @@ export type SignUpResponse = {
   token: string
   user: User
 }
-
-export const buildSignUpRoute = (fastify: FastifyInstance) => ({
-  method: "POST" as HTTPMethods,
-  url: "/auth/sign-up",
-  schema: {
-    body: {
-      type: "object",
-      properties: {
-        username: { type: "string" },
-        password: { type: "string" },
-      },
-      required: ["username", "password"],
-    },
-  },
-  handler: async (request: FastifyRequest<{ Body: SignUpRequest; Reply: SignUpResponse }>, reply: FastifyReply) => {
-    request.log.info("Signing up")
-    const { username, password } = request.body
-    // todo: encrypt password
-
-    const user = await fastify.prisma.user.create({
-      data: {
-        username,
-        password,
-      },
-    })
-
-    request.log.info("User created")
-
-    fastify.jwt.sign({ id: user.id }, (err: Error | null, token: string) => {
-      if (err) {
-        reply.code(500).send({ message: "Error signing up" })
-      } else {
-        reply.send({ token, user })
-      }
-    })
-  },
-})
 
 export const signUpOptions = {
   schema: {
